077402855dcd43c7276fc4f62277a3c9e5bbfa2e
aimebertrand <aime.bertrand@epitech.eu>
2023-04-15 16:48:36 +0200

    [ADDING]  Création du projet via Maven #2 - ajout des dependances spring et d'un logger, l'application démarre bien et affiche un message

---
+++ src/test/java/com/mycompany/app/oil202223td2shadowteam/Oil202223Td2ShadowTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.app.oil202223td2shadowteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oil202223Td2ShadowTeamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
