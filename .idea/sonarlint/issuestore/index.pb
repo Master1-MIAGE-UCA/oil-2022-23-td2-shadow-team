9a4668a931264cb2d6300805b6a3bacad0f57846
aimebertrand <aime.bertrand@epitech.eu>
2023-04-25 00:43:02 +0200

    [ADDING] Symbolic springboot web service

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+3
+git,4/6/46f1a0bd5592a2f9244ca321b129902a06b53e03
+4
+mvnw,5/d/5d837c4ed85ecaaf932c506e80ff5d7b9f3d590d
+;
+.travis.yml,d/e/dea01dd89a3b602828e630677fde5d77c06441c8
+B
+docker-compose.yml,3/5/35b8c13cf2eb2a194eada000eb310d65aed53b2a
--- partie/src/main/java/partie/Partie.java
+++ partie/src/main/java/partie/Partie.java
@@ -2,12 +2,22 @@ package partie;
 
 import hebergeur.hebergeur;
 import appariement.appariement;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Partie {
 
-    public final static void main(String [] args) {
-        System.out.println("Hello World From Partie");
+    private static final Logger logger = LogManager.getLogger(Partie.class);
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Partie.class, args);
         appariement.main(args);
         hebergeur.main(args);
+        logger.info("Application started. Lessgo ! ");
+
     }
 }
