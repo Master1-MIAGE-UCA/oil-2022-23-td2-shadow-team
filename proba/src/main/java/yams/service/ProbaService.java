1318776c60a99f94ab7247c58c852e4a12161e7a
Mouctar <47394031+mamadou-mouctar-diallo@users.noreply.github.com>
2023-05-06 20:03:04 +0200

    Définir une méthode pour calculer la probabilité de chaque combinaison en fonction des dés lancés

---
+++ proba/src/main/java/yams/service/ProbaService.java
@@ -0,0 +1,35 @@
+package yams.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@AllArgsConstructor
+@Service
+public class ProbaService {
+    public Optional<Double> brelan(){
+        return Optional.of(0.0);
+    }
+    public Optional<Double> full(){
+        return Optional.of(0.0);
+    }
+    public Optional<Double> smallStraight(){
+        return Optional.of(0.0);
+    }
+    public Optional<Double> largeStraight(){
+        return Optional.of(0.0);
+    }
+    public Optional<Double> yams(){
+        return Optional.of(0.0);
+    }
+    public Optional<Double> square(){
+        return Optional.of(0.0);
+    }
+    public Optional<Double> noCombinaison(){
+        return Optional.of(0.0);
+    }
+    private double paire(){
+        return 0.0;
+    }
+}
